626003c4633a00435027bb1c78b20ebbf3c362fc
broadwells <broadwell.s@gmail.com>
2018-07-25 21:06:03 -0400

    Added condition for craps, boxcars, and losing

--- dice.js
+++ dice.js
@@ -19,9 +19,15 @@ console.log(strResult + dice2);
 var total = dice1 + dice2;
 console.log("Total roll: " + total);
 if (dice1 == 1 && dice2 == 1) {
-  console.log("You got snake eyes!");
+  console.log("Snake eyes!");
+} else if ((dice1 + dice2) == 7 || (dice1 + dice2) == 11) {
+  console.log("Craps!");
+} else if ((dice1 + dice2) == 12) {
+  console.log("Boxcars!");
 } else if (dice1 == dice2) {
-  console.log("You got doubles!");
+  console.log("Doubles!");
+} else {
+  console.log("You lose.");
 }
 
 console.log("Goodbye!");
